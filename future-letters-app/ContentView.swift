8975bf90e47f2977565ade206d193f5e5b0f1609
shuang.yang <shuang.yang@woquth.com>
2025-01-15 09:08:36 +0800

    Initial Commit

---
+++ future-letters-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  future-letters-app
+//
+//  Created by admin on 2025/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ future-letters-app/future_letters_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  future_letters_appApp.swift
+//  future-letters-app
+//
+//  Created by admin on 2025/1/15.
+//
+
+import SwiftUI
+
+@main
+struct future_letters_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
